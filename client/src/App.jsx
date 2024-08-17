a3758cfeb039daec818b7d08e291f5d30bab3ce7
Infy <juniorbilogue@gmail.com>
2024-08-17 16:10:53 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* Other content */}
+    </div>
+  );
+}
+
+export default App;
