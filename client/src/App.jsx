1f5da03ea884a4141c141e06de9d80a44c6249bb
Infy <juniorbilogue@gmail.com>
2024-08-24 20:53:39 +0100

    #stage1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Carousel from './components/Carousel'
+import Solution from './components/Solution'
+import Accomplishments from './components/Accomplishments'
+import Team from './components/Team'
+import Gallery from './components/Gallery'
+import ContactForm from './components/ContactForm'
+import Footer from './components/Footer'
+import Service from './components/Service'
+import Technology from './components/Technology'
+
 
 export default function App() {
   return (
    <div>
     <Navbar/>
+    <Carousel/>
+    <Solution/>
+    <Service/>
+    <Technology/>
+    <Accomplishments/>
+    <Team/>
+    <Gallery/>
+    <ContactForm/>
+    <Footer/>
    </div>
   )
 }
